261c30d4797da28ab870b16afe259ba1e036ed91
leopo <leopoldo.pollini.studenti@isii.it>
2025-09-23 09:20:19 +0200

    Added a FolderBrowser to upload the .Json file, Added the Json reader, Added the insterction of the Json obj in an adapptive table with SQLite

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_UploadFolder = new System.Windows.Forms.Button();
+            this.btn_RemoveFolder = new System.Windows.Forms.Button();
+            this.btn_AddToData = new System.Windows.Forms.Button();
+            this.lsv_FilesPath = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_SubmitQuery = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_UploadFolder
+            // 
+            this.btn_UploadFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_UploadFolder.Location = new System.Drawing.Point(12, 469);
+            this.btn_UploadFolder.Name = "btn_UploadFolder";
+            this.btn_UploadFolder.Size = new System.Drawing.Size(377, 23);
+            this.btn_UploadFolder.TabIndex = 0;
+            this.btn_UploadFolder.Text = "Upload";
+            this.btn_UploadFolder.UseVisualStyleBackColor = true;
+            this.btn_UploadFolder.Click += new System.EventHandler(this.btn_UploadFolder_Click);
+            // 
+            // btn_RemoveFolder
+            // 
+            this.btn_RemoveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_RemoveFolder.Enabled = false;
+            this.btn_RemoveFolder.Location = new System.Drawing.Point(12, 500);
+            this.btn_RemoveFolder.Name = "btn_RemoveFolder";
+            this.btn_RemoveFolder.Size = new System.Drawing.Size(377, 23);
+            this.btn_RemoveFolder.TabIndex = 1;
+            this.btn_RemoveFolder.Text = "Remove";
+            this.btn_RemoveFolder.UseVisualStyleBackColor = true;
+            this.btn_RemoveFolder.Click += new System.EventHandler(this.btn_RemoveFolder_Click);
+            // 
+            // btn_AddToData
+            // 
+            this.btn_AddToData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_AddToData.Enabled = false;
+            this.btn_AddToData.Location = new System.Drawing.Point(12, 529);
+            this.btn_AddToData.Name = "btn_AddToData";
+            this.btn_AddToData.Size = new System.Drawing.Size(377, 38);
+            this.btn_AddToData.TabIndex = 2;
+            this.btn_AddToData.Text = "Confirm";
+            this.btn_AddToData.UseVisualStyleBackColor = true;
+            this.btn_AddToData.Click += new System.EventHandler(this.btn_AddToData_Click);
+            // 
+            // lsv_FilesPath
+            // 
+            this.lsv_FilesPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lsv_FilesPath.HideSelection = false;
+            this.lsv_FilesPath.Location = new System.Drawing.Point(12, 12);
+            this.lsv_FilesPath.MultiSelect = false;
+            this.lsv_FilesPath.Name = "lsv_FilesPath";
+            this.lsv_FilesPath.Size = new System.Drawing.Size(377, 451);
+            this.lsv_FilesPath.TabIndex = 3;
+            this.lsv_FilesPath.UseCompatibleStateImageBehavior = false;
+            this.lsv_FilesPath.View = System.Windows.Forms.View.List;
+            this.lsv_FilesPath.SelectedIndexChanged += new System.EventHandler(this.lsv_FilesPath_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(396, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(803, 507);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(396, 537);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(662, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btn_SubmitQuery
+            // 
+            this.btn_SubmitQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_SubmitQuery.Location = new System.Drawing.Point(1064, 529);
+            this.btn_SubmitQuery.Name = "btn_SubmitQuery";
+            this.btn_SubmitQuery.Size = new System.Drawing.Size(135, 38);
+            this.btn_SubmitQuery.TabIndex = 6;
+            this.btn_SubmitQuery.Text = "Search";
+            this.btn_SubmitQuery.UseVisualStyleBackColor = true;
+            this.btn_SubmitQuery.Click += new System.EventHandler(this.btn_SubmitQuery_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1211, 579);
+            this.Controls.Add(this.btn_SubmitQuery);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lsv_FilesPath);
+            this.Controls.Add(this.btn_AddToData);
+            this.Controls.Add(this.btn_RemoveFolder);
+            this.Controls.Add(this.btn_UploadFolder);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btn_UploadFolder;
+        private System.Windows.Forms.Button btn_RemoveFolder;
+        private System.Windows.Forms.Button btn_AddToData;
+        private System.Windows.Forms.ListView lsv_FilesPath;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_SubmitQuery;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,298 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LogTool
 {
     public partial class Form1 : Form
     {
+        public List<string> filespath = new List<string>();
+        private string dbPath = "logs.db";
+        private SQLiteConnection conn;
         public Form1()
         {
             InitializeComponent();
+            InitDatabase();
+        }
+
+
+        private void InitDatabase()
+        {
+            bool createNew = !File.Exists(dbPath);
+
+            conn = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            conn.Open();
+
+            if (createNew)
+            {
+                using (var cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandText = @"
+                CREATE TABLE Logs (
+                    [time] TEXT
+                );";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            dataGridView1.DataSource = LoadTable("Logs");
+        }
+
+        private void btn_UploadFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        foreach (var path in Directory.GetFiles(fbd.SelectedPath))
+                        {
+                            filespath.Add(path);
+                            lsv_FilesPath.Items.Add(path);
+                        }
+                    }
+                }
+                btn_AddToData.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_RemoveFolder_Click(object sender, EventArgs e)
+        {
+            filespath.Remove(lsv_FilesPath.SelectedItems[0].Text);
+            lsv_FilesPath.Items.Remove(lsv_FilesPath.SelectedItems[0]);
+            if (lsv_FilesPath.Items.Count > 0)
+            {
+                btn_AddToData.Enabled = true;
+            }
+            else
+            {
+                btn_AddToData.Enabled = false;
+            }
+        }
+
+        private void lsv_FilesPath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsv_FilesPath.SelectedItems.Count > 0) 
+            {
+                btn_RemoveFolder.Enabled = true;
+            }
+            else
+            {
+                btn_RemoveFolder.Enabled = false;
+            }
+
+        }
+
+        private void btn_AddToData_Click(object sender, EventArgs e)
+        {
+            foreach (var path in filespath)
+            {
+                var lines = ReadJsonObjects(path);
+                foreach (var Json in lines)
+                {
+                    string fixedJson = FixInvalidJson(Json);
+                    var obj = JObject.Parse(fixedJson);
+
+                    EnsureColumnsExist(obj); // Assicura che tutte le colonne esistano
+
+                    string query = JsonToQuery(obj);
+
+                    using (var cmd = new SQLiteCommand(query, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            dataGridView1.DataSource = LoadTable("Logs");
+            filespath.Clear();
+            lsv_FilesPath.Clear();
+        }
+
+        private string JsonToQuery(JObject obj)
+        {
+            var columns = obj.Properties().Select(p => $"[{p.Name}]").ToList();
+            var values = obj.Properties().Select(p => p.Value.ToString().Replace("'", "''")).ToList();
+
+            string query = $"INSERT INTO Logs ({string.Join(",", columns)}) VALUES ('{string.Join("','", values)}');";
+            return query;
+        }
+
+        private IEnumerable<string> ReadJsonObjects(string path)
+        {
+            var sb = new StringBuilder();
+            int braceCount = 0;
+
+            foreach (var line in File.ReadLines(path))
+            {
+                foreach (char c in line)
+                {
+                    if (c == '{') braceCount++;
+                    if (c == '}') braceCount--;
+                }
+
+                sb.AppendLine(line);
+
+                if (braceCount == 0 && sb.Length > 0)
+                {
+                    // JSON completo
+                    yield return sb.ToString().Trim();
+                    sb.Clear();
+                }
+            }
+
+            // in caso ci fosse qualcosa rimasto senza chiusura corretta
+            if (sb.Length > 0)
+            {
+                yield return sb.ToString().Trim();
+            }
+        }
+
+        private string FixInvalidJson(string line)
+        {
+            line = line.Trim();
+
+            if (line.Contains("\"message\":"))
+            {
+                int idx = line.IndexOf("\"message\":");
+                int start = idx + "\"message\":".Length;
+
+                // Prendi tutto ciò che segue fino all'ultima graffa chiusa
+                int end = line.LastIndexOf('}');
+                if (end < start) end = line.Length;
+
+                string before = line.Substring(0, start);
+                string message = line.Substring(start, end - start).Trim();
+
+                // escape eventuali doppi apici interni
+                message = message.Replace("\\", "\\\\").Replace("\"", "\\\"");
+
+                // la graffa finale viene inclusa correttamente
+                
+                string after = "}"; // include la }
+
+                // aggiunge virgolette attorno al valore del messaggio
+                line = before + "\"" + message + "\"" + after;
+            }
+            // -------------------------
+            // 2️⃣ Fix per "query-string"
+            // -------------------------
+            if (line.Contains("\"query-string\":"))
+            {
+                int idx = line.IndexOf("\"query-string\":");
+                int start = idx + "\"query-string\":".Length;
+
+                // trova la prima virgola dopo query-string oppure la fine della riga
+                int commaIdx = line.IndexOf(',', start);
+                int end = (commaIdx > 0) ? commaIdx : line.Length;
+
+                string before = line.Substring(0, start);
+                string qsValue = line.Substring(start, end - start).Trim();
+
+                // escape eventuali doppi apici interni
+                qsValue = qsValue.Replace("\\", "\\\\").Replace("\"", "\\\"");
+
+                string after = line.Substring(end);
+
+                // aggiunge virgolette intorno al valore di query-string
+                line = before + "\"" + qsValue + "\"" + after;
+            }
+
+            return line;
+        }
+
+
+        private DataTable LoadTable(string tableName)
+        {
+            var dt = new DataTable();
+
+            using (var cmd = new SQLiteCommand($"SELECT * FROM {tableName} ORDER BY [time] LIMIT 100", conn))
+            using (var adapter = new SQLiteDataAdapter(cmd))
+            {
+                adapter.Fill(dt);
+            }
+
+            return dt;
+        }
+
+        private void EnsureColumnsExist(JObject obj)
+        {
+            foreach (var prop in obj.Properties())
+            {
+                string columnName = prop.Name;
+
+                // Verifica se la colonna esiste già
+                using (var cmd = new SQLiteCommand($"PRAGMA table_info(Logs);", conn))
+                {
+                    var reader = cmd.ExecuteReader();
+                    bool exists = false;
+                    while (reader.Read())
+                    {
+                        if (reader["name"].ToString() == columnName)
+                        {
+                            exists = true;
+                            break;
+                        }
+                    }
+                    reader.Close();
+
+                    // Se non esiste, aggiungila
+                    if (!exists)
+                    {
+                        using (var addCmd = new SQLiteCommand($"ALTER TABLE Logs ADD COLUMN [{columnName}] TEXT;", conn))
+                        {
+                            addCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btn_SubmitQuery_Click(object sender, EventArgs e)
+        {
+            string userQuery = textBox1.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(userQuery))
+            {
+                MessageBox.Show("Inserisci una query.");
+                return;
+            }
+
+            try
+            {
+                var dt = new DataTable();
+
+                using (var cmd = new SQLiteCommand(userQuery, conn))
+                using (var adapter = new SQLiteDataAdapter(cmd))
+                {
+                    adapter.Fill(dt);
+                }
+
+                // Aggiorna il DataGridView con i risultati
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore nella query: " + ex.Message);
+            }
         }
     }
 }
